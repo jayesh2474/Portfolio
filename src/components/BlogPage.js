447b82d0c9df659fd9a92802c66a4408ee58ab07
Jayesh Joshi <divyeshjoshi7474@gmail.com>
2024-05-11 19:28:16 +0530

    media_query_improve

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -38,7 +38,11 @@ padding-top: 10rem;
 const Grid = styled.div`
 display: grid;
 grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
-grid-gap: calc(1rem + 2vw);
+  grid-gap: calc(1rem + 2vw);
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr; // Adjust for mobile
+  }
 `
 
 // Framer-motion config
